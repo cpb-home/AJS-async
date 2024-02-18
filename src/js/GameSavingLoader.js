4cc7efc3df692c5d6e410f74fd26d57b0e24c7b4
cpb-home <cpb@bizb.ru>
2024-02-18 18:34:38 +0300

    task done

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -3,18 +3,12 @@ import read from './reader';
 import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
-  static load() {
-    //const data = read(); // возвращается Promise!
-    //const value = json(data); // возвращается Promise!
+  static async load() {
 
-    return read()
-      .then(buf => json(buf))
-      .then(res => {
-        const gs = JSON.parse(res);
-        return new GameSaving(gs.id, gs.created, gs.userInfo);
-      })
-      .catch(e => {
-        throw new Error(`Ошибка ${e}`);
-      });
+    const data = await read();
+    const value = await json(data);
+    const obj = JSON.parse(value);
+
+    return new GameSaving(obj.id, obj.created, obj.userInfo);
   }
 }
--- src/js/__tests__/GameSavingLoader.test.js
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -5,12 +5,3 @@ test('testing GameSavingLoader class load method', async () => {
   expect(data).toEqual({"created": 1546300800, "id": 9, "userInfo": {"id": 1, "level": 10, "name": "Hitman", "points": 2000}});
 });
 
-test('testing GameSavingLoader class load method error reply', async () => {
-  expect.assertions(1);
-  try {
-    const data = await GameSavingLoader.load();
-  }
-  catch(e) {
-    expect(e.name).toEqual('Error');
-  }
-});
